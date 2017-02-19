bbcf5d90d9b81b5c3859b9143ce4b4b7cb49c908
jon <jonathan.lazarini@wipro.com>
2017-02-19 23:45:16 +0000

    add 1 level of security against XSS attacks by sanatizing preloaded state

--- bin/server.js
+++ bin/server.js
@@ -8,11 +8,11 @@ import React from 'react'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import { renderToString } from 'react-dom/server'
-// import counterApp from '../src/reducers'
-// import App from '../src/containers/App'
+
 import AppContainer from '../src/containers/AppContainer'
 import { fetchCounter } from '../api/counter'
 import reducers from '../src/reducers';
+
 
 const app = Express()
 const port = 3000
@@ -21,7 +21,6 @@ const port = 3000
 app.use(handleRender)
 
 
-// We are going to fill these out in the sections to follow
 function handleRender(req, res) {
 	// Query our mock API asynchronously
 	fetchCounter(apiResult => {
@@ -63,7 +62,7 @@ function renderFullPage(html, preloadedState) {
         <script>
           // WARNING: See the following for Security isues with this approach:
           // http://redux.js.org/docs/recipes/ServerRendering.html#security-considerations
-          window.__PRELOADED_STATE__ = ${JSON.stringify(preloadedState)}
+          window.__PRELOADED_STATE__ = ${JSON.stringify(preloadedState).replace(/</g, '\\u003c')}
         </script>
         <script src="./index.js"></script>
       </body>
@@ -73,6 +72,8 @@ function renderFullPage(html, preloadedState) {
 
 /**
  * script is where the bundled file will be served
+ * sanitizing preloadedState for XSS attacks
+ * if more complex, serialize json with https://github.com/yahoo/serialize-javascript
  */
 
 app.listen(port)
