178c7ec0b1f681c3777a537e4dae18463513c339
Jon Lazarini <jonathan.lazarini@wipro.com>
2018-03-22 11:27:59 +0000

    add sourcemaps, add data serialization and pipe error to client in middleware

--- server/app.js
+++ server/app.js
@@ -6,9 +6,16 @@ import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import cors from 'cors'
 import { StaticRouter, matchPath } from 'react-router-dom'
+import serialize from 'serialize-javascript'
+import sourceMapSupport from 'source-map-support'
 import reducers from '../src/reducers/'
 import App from '../src/app'
 import routes from '../src/routes'
+
+
+if (process.env.NODE_ENV === "development") {
+  sourceMapSupport.install();
+}
 
 
 const app = express();
@@ -16,14 +23,15 @@ const app = express();
 app.use(express.static('public'))
 
 app.use(cors())
-app.get('*', handleRender)
+app.use(handleRender)
 
-function handleRender(req, res) {
+function handleRender(req, res, next) {
   const currentRoute = routes.find(route => matchPath(req.url, route))
 
   let initialState = currentRoute.component.getInitialData()
   Promise.resolve(initialState)
     .then(data => {
+      // throw new Error('boom!')
       const context = {data}
       const html = renderToString(
         <StaticRouter location={req.url} context={context}>
@@ -32,7 +40,8 @@ function handleRender(req, res) {
       )
       res.send(renderFullPage(html, data))
     })
-    .catch(err => console.error(err))
+    // sends error from server to the client view - ** SHOULD BE DEV ONLY **
+    .catch(next)
 }
 
 const renderFullPage = (tmpl, state) => `
@@ -49,7 +58,7 @@ const renderFullPage = (tmpl, state) => `
         <script>
         // WARNING: See the following for security issues around embedding JSON in HTML:
         // http://redux.js.org/docs/recipes/ServerRendering.html#security-considerations
-        window.__PRELOADED_STATE__ = ${JSON.stringify(state).replace(/</g, '\\u003c')}
+        window.__PRELOADED_STATE__ = ${serialize(state)}
         </script>
       </body>
     </html>
