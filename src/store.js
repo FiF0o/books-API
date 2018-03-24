0dff934b1f9cdfcd1a95f9d56cfb0865c567a7f5
Jon Lazarini <jonathan.lazarini@wipro.com>
2018-03-24 01:30:04 +0000

    add redux tool

--- src/store.js
+++ src/store.js
@@ -1,17 +1,27 @@
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import createSagaMiddleware from 'redux-saga'
 
 import reducers from './reducers'
 
 
+const composeEnhancers =
+typeof window === 'object' &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+  }) : compose;
 const sagaMiddleware = createSagaMiddleware()
+
+
+const middleware = [
+  sagaMiddleware
+]
 
 
 const configureStore = preloadedState =>
 createStore(
   reducers,
   preloadedState,
-  applyMiddleware(sagaMiddleware)
+  composeEnhancers(applyMiddleware(...middleware))
 )
 
 
