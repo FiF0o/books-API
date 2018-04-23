d8f0d5f6df0a6451a1bafe91dc8e7ef541f1e356
Jon Lazarini <jonathan.lazarini@wipro.com>
2018-04-23 14:02:40 +0100

    add getUsers query, login mutation for auth

--- api/resolvers/Mutation.js
+++ api/resolvers/Mutation.js
@@ -1,15 +1,17 @@
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
-const {APP_SECRET} = require('../utils')
+const {APP_SECRET, getUserId} = require('../utils')
 
 
 function postBook (root, args, context, info) {
+  const userId = getUserId(context)
   return context.db.mutation.createBook({
     data: {
       author: args.author,
       title: args.title,
       description: args.description,
-      favorite: args.favorite
+      favorite: args.favorite,
+      postedBy: { connect: {id: userId }}
     }
   }, info)
 }
@@ -28,8 +30,28 @@ async function signup(parent, args, context, info) {
   }
 }
 
+async function login(parent, args, context, info) {
+  const user = await context.db.query.user({ where: { email: args.email } }, ` { id password } `)
+  if (!user) {
+    throw new Error('No such user found')
+  }
+
+  const valid = await bcrypt.compare(args.password, user.password)
+  if (!valid) {
+    throw new Error('Invalid password')
+  }
+
+  const token = jwt.sign({ userId: user.id }, APP_SECRET)
+
+  return {
+    token,
+    user,
+  }
+}
+
 
 module.exports = {
   postBook,
-  signup
+  signup,
+  login
 }
--- api/resolvers/Query.js
+++ api/resolvers/Query.js
@@ -1,10 +1,15 @@
-function getBooks(root, args, context, info) {
-  return context.db.query.books({}, info)
+async function getBooks(root, args, context, info) {
+  return await context.db.query.books({}, info)
+}
+
+async function getUsers(root, args, context, info) {
+  return await context.db.query.users({}, info)
 }
 
 const info = () => `Hello!`
 
 module.exports = {
   getBooks,
-  info
+  info,
+  getUsers
 }
