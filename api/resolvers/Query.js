e811af92f730ab10979e8ed97199640561ce898b
Jon Lazarini <jonathan.lazarini@wipro.com>
2018-04-23 14:49:41 +0100

    add filter on Book

--- api/resolvers/Query.js
+++ api/resolvers/Query.js
@@ -1,5 +1,17 @@
 async function getBooks(root, args, context, info) {
-  return await context.db.query.books({}, info)
+  // constructing the filtered query for filter Prisma binding converted into GraphQL mutation
+  const where = args.filter ?
+    {
+      OR: [
+        { author_contains: args.filter },
+        { title_contains: args.filter },
+        { description_contains: args.filter }
+      ]
+    }
+    :
+    {}
+  // query ($filter: String) { books(where:{...}) }
+  return await context.db.query.books({ where }, info)
 }
 
 async function getUsers(root, args, context, info) {
